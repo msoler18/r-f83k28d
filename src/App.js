8804e735977f183a31dd66bb44c6c5b18bfd97ff
msoler18 <msoler735@gmail.com>
2018-05-08 21:53:48 -0500

    Solver Challenge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      counter: 0
+    }
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.counter}</span>
+        <button id="inc" onClick={this.increment.bind(this)}>Incrementa</button>
       </div>
     );
+  }
+  increment(counter){
+   this.setState({
+    counter : this.state.counter + 1
+   })
   }
 }
 
